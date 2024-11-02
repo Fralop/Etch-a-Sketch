50882420e246a872f2d51e861d7b470494d0f385
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-02 17:43:09 -0400

    Found an issue

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+// Click even including action function
+const btn = document.querySelector('button');
+btn.addEventListener('click', clickEffect);
+let x;
+function clickEffect () {
+   let userInput = prompt ("How many squares per side for the new grid?");
+   x = Number(userInput);
+}
+// Grill functions
+while (x>0) {
 const container = document.querySelector('.container');
-let x = 16;
-let y = 16;
 for (let i=0; i<x; i++) {
     const rowDiv = document.createElement('div');
-    for (let j=0; j<y; j++) {
+    for (let j=0; j<x; j++) {
         const colDiv = document.createElement('div');
         colDiv.classList.add('column');
         rowDiv.appendChild(colDiv);
     }
     rowDiv.classList.add('row');
     container.appendChild(rowDiv);
+}
 }
 // Mousehover event including action function
 const divs = document.querySelectorAll('.column');
@@ -20,10 +29,3 @@ divs.forEach((div) => {
 function hoverEffect (e) {
     e.target.classList.add('over');
 } 
-// Click even including action function
-const btn = document.querySelector('button');
-btn.addEventListener('click', clickEffect);
-
-function clickEffect () {
-    prompt gi
-}
