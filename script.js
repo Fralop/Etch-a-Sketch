7fa91b6da2e1f2e7b29ae1f5d89c0161becbae59
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-02 17:27:26 -0400

    Added some style improvements

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let i=0; i<x; i++) {
     rowDiv.classList.add('row');
     container.appendChild(rowDiv);
 }
-
+// Mousehover event including action function
 const divs = document.querySelectorAll('.column');
 divs.forEach((div) => {
     div.addEventListener('mouseover', hoverEffect);
@@ -20,3 +20,10 @@ divs.forEach((div) => {
 function hoverEffect (e) {
     e.target.classList.add('over');
 } 
+// Click even including action function
+const btn = document.querySelector('button');
+btn.addEventListener('click', clickEffect);
+
+function clickEffect () {
+    prompt gi
+}
