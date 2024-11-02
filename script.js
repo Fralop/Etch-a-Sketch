13fe939dcc48b52348df6fcb17fed9416a24ceff
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-02 16:51:34 -0400

    Got the mouseover event working ! yay !

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const container = document.querySelector('.container');
-let x = 3;
-let y = 5;
+let x = 16;
+let y = 16;
 for (let i=0; i<x; i++) {
     const rowDiv = document.createElement('div');
     for (let j=0; j<y; j++) {
         const colDiv = document.createElement('div');
-        rowDiv.appendChild(colDiv);
         colDiv.classList.add('column');
+        rowDiv.appendChild(colDiv);
     }
     rowDiv.classList.add('row');
     container.appendChild(rowDiv);
 }
+
+const divs = document.querySelectorAll('.column');
+divs.forEach((div) => {
+    div.addEventListener('mouseover', hoverEffect);
+});
+
+function hoverEffect (e) {
+    e.target.classList.add('over');
+} 
