5e0d1a1aa539b65455e7fb934974b84bb8c06f67
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-03 09:49:02 -0500

    Corrected the file by moving the declaration of div with class container from the beginning to where it was really needed within the createGrid function.

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-// Click even including action function
+const body = document.querySelector('body');
 const btn = document.querySelector('button');
-btn.addEventListener('click', askUserInput);
 
-function askUserInput () {
+// Click even including action function
+btn.addEventListener('mousedown', clearGrid);
+btn.addEventListener('mouseup', createDiv);
+
+//ClearGrid
+function clearGrid() {
+    const div = document.querySelector('div');
+    body.removeChild(div);
+}
+
+function createDiv() {
+   const div = document.createElement ('div');
+   div.classList.add('container');
+   body.appendChild(div);
+   askUserInput(); 
+}
+
+function askUserInput() {
    let x = 0;
    let userInput = prompt ("How many squares per side for the new grid? (Less than 100)");
    x = Number(userInput);
    if (x<=100) {
-   createGrid(x);
-   btn.removeEventListener('click', askUserInput);
+        createGrid(x);
    } else alert ('Please enter a number lower than 100');
 }
+
 // Grill functions
 function createGrid (x) {
-    const container = document.querySelector('.container');
     for (let i=0; i<x; i++) {
         const rowDiv = document.createElement('div');
         for (let j=0; j<x; j++) {
@@ -22,17 +37,19 @@ function createGrid (x) {
             rowDiv.appendChild(colDiv);
         }
         rowDiv.classList.add('row');
+        const container = document.querySelector('.container');
         container.appendChild(rowDiv);
     }
-    paintEffect ();
+    paintEffect();
 }
 // Mousehover event including action function
-function paintEffect () {
+function paintEffect() {
     const divs = document.querySelectorAll('.column');
     divs.forEach((div) => {
         div.addEventListener('mouseover', hoverEffect);
     });
-    function hoverEffect (e) {
-        e.target.classList.add('over');
-    } 
 }
+
+function hoverEffect(e) {
+    e.target.classList.add('over');
+} 
