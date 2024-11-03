6d15f78312f5937c16734f4edf04a5af11a2ddd3
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-03 00:21:32 -0400

    Divided the code in functions to ensure order of execution and ensured the userInput is less than 100

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 // Click even including action function
 const btn = document.querySelector('button');
-btn.addEventListener('click', clickEffect);
-let x;
-function clickEffect () {
-   let userInput = prompt ("How many squares per side for the new grid?");
+btn.addEventListener('click', askUserInput);
+
+function askUserInput () {
+   let x = 0;
+   let userInput = prompt ("How many squares per side for the new grid? (Less than 100)");
    x = Number(userInput);
+   if (x<=100) {
+   createGrid(x);
+   btn.removeEventListener('click', askUserInput);
+   } else alert ('Please enter a number lower than 100');
 }
 // Grill functions
-while (x>0) {
-const container = document.querySelector('.container');
-for (let i=0; i<x; i++) {
-    const rowDiv = document.createElement('div');
-    for (let j=0; j<x; j++) {
-        const colDiv = document.createElement('div');
-        colDiv.classList.add('column');
-        rowDiv.appendChild(colDiv);
+function createGrid (x) {
+    const container = document.querySelector('.container');
+    for (let i=0; i<x; i++) {
+        const rowDiv = document.createElement('div');
+        for (let j=0; j<x; j++) {
+            const colDiv = document.createElement('div');
+            colDiv.classList.add('column');
+            rowDiv.appendChild(colDiv);
+        }
+        rowDiv.classList.add('row');
+        container.appendChild(rowDiv);
     }
-    rowDiv.classList.add('row');
-    container.appendChild(rowDiv);
-}
+    paintEffect ();
 }
 // Mousehover event including action function
-const divs = document.querySelectorAll('.column');
-divs.forEach((div) => {
-    div.addEventListener('mouseover', hoverEffect);
-});
-
-function hoverEffect (e) {
-    e.target.classList.add('over');
-} 
+function paintEffect () {
+    const divs = document.querySelectorAll('.column');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', hoverEffect);
+    });
+    function hoverEffect (e) {
+        e.target.classList.add('over');
+    } 
+}
